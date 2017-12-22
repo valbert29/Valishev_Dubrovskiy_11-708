587baa19fe0f337452a140f8c9ae9993409752ce
Andermaid <31659731+Andermaid@users.noreply.github.com>
2017-12-22 21:13:47 +0300

    Семестровка №3 Игра "Волк ловит яйца"

---
+++ EggLines.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+namespace WolfCatchEggs
+{
+    //Работа Дубровского Егора
+    class EggLines
+    {
+        public static char[][] Lines = new char[][] { new char[6], new char[6], new char[6], new char[6] };
+
+        public static void Initialize()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 6; j++)
+                    Lines[i][j] = ' ';
+        }
+
+        public static void RunEgg(char[][] lines)
+        {
+            Random random = new Random();
+            int k = 0;
+            while (Game.Lives > 0 && Game.Points < 1000)
+            {
+                if (k % 2 == 0) lines[random.Next(4)][0] = 'O';
+                Print(lines);
+                for (int i = 0; i < 4; i++)
+                    for (int j = 5; j >= 0; j--)
+                        if (lines[i][j] == 'O')
+                        {
+                            lines[i][j] = ' ';
+                            if (j != 5) lines[i][j + 1] = 'O';
+                            else
+                            {
+                                ChangeResult(Game.WolfPosition, i);
+                                PrintPoints();
+                            }
+                        }
+                if (Game.Points <= 150) Thread.Sleep(1200 - (Game.Points * 6));
+                else Thread.Sleep(300);
+                k++;
+            }
+        }
+
+        public static void ChangeResult(int wolfPosition, int lineNumber)
+        {
+            if (lineNumber == wolfPosition) Game.Points++;
+            else Game.Lives--;
+        }
+
+        public static void Print(char[][] lines)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int x;
+                int y;
+                int n;
+                if (i % 2 == 0)
+                {
+                    x = 0;
+                    n = 2;
+                }
+                else
+                {
+                    x = 31;
+                    n = -2;
+                }
+                if (i < 2) y = 1;
+                else y = 5;
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.SetCursorPosition(x + (n * j) + 3, y + j);
+                    Console.Write(lines[i][j]);
+                }
+            }
+        }
+
+        public static void PrintPoints()
+        {
+            Console.SetCursorPosition(10, 12);
+            Console.Write(Game.Points);
+            Console.SetCursorPosition(35, 12);
+            Console.Write(Game.Lives);
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+
+namespace WolfCatchEggs
+{
+    //Работа просто Валишева Альберта
+    public class Game
+    {
+        public static int WolfX = 0;
+        public static int WolfY = 0;
+        public static int Lives;
+        public static int WolfPosition;
+        public static int Points;
+
+        static void Main()
+        {
+            Console.BufferWidth = Console.WindowWidth = 38;
+            Console.BufferHeight = Console.WindowHeight = 15;
+            Console.CursorVisible = false;
+            Console.Write("Управление Q, E, A, D\n" +
+                          "Press S to Begin");
+            while(true)
+                if (Console.ReadKey(true).Key == ConsoleKey.S) GameProcess();
+        }
+
+        public static void GameProcess()
+        {
+            Points = 0;
+            Lives = 3;
+            Console.Clear();
+            Console.Write(@"                                     " + "\n" +
+                          @"                                     " + "\n" +
+                          @" Q \_                            _/ E" + "\n" +
+                          @"     \_                        _/    " + "\n" +
+                          @"       \_                    _/      " + "\n" +
+                          @"         \_                _/        " + "\n" +
+                          @" A \_            ____            _/ D" + "\n" +
+                          @"     \_       \_|""  ""|_/       _/    " + "\n" +
+                          @"       \_       | __ |       _/      " + "\n" +
+                          @"         \_   _/|____|\_   _/        " + "\n" +
+                          @"                |/  \|               " + "\n" +
+                          @"                |    |               " + "\n" +
+                          @" Points - 0     |    |     Lives - 3 " + "\n");
+            Thread WolfMove = new Thread(GetWolfPosition);
+            WolfMove.Start();
+            Move(ConsoleKey.Q);
+            EggLines.Initialize();
+            EggLines.RunEgg(EggLines.Lines);
+            WolfMove.Abort();
+            Console.SetCursorPosition(1, 13);
+            if (Lives == 0) Console.Write("Game Over(");
+            else Console.Write("Congratulations)");
+            Console.WriteLine("\n Press S to retry");
+            while (true)
+                if (Console.ReadKey(true).Key == ConsoleKey.S) GameProcess();
+        }
+
+        static void GetWolfPosition()
+        {
+            while (true)
+                Move(Console.ReadKey(true).Key);
+        }
+
+        public static void Move(ConsoleKey position)
+        {
+            Console.SetCursorPosition(WolfX, WolfY);
+            Console.Write("   ");
+            switch (position)
+            {
+                case ConsoleKey.Q:
+                    WolfX = 11;
+                    WolfY = 6;
+                    WolfPosition = 0;
+                    break;
+                case ConsoleKey.E:
+                    WolfX = 24;
+                    WolfY = 6;
+                    WolfPosition = 1;
+                    break;
+                case ConsoleKey.A:
+                    WolfX = 11;
+                    WolfY = 10;
+                    WolfPosition = 2;
+                    break;
+                case ConsoleKey.D:
+                    WolfX = 24;
+                    WolfY = 10;
+                    WolfPosition = 3;
+                    break;
+            }
+            Console.SetCursorPosition(WolfX, WolfY);
+            Console.Write(@"\_/");
+        }
+    }
+}
